e0f080a99c9f2b9958fea1609065e9271ab3af98
etranger2268 <shun.5.2268@gmail.com>
2025-12-06 00:28:18 +0900

    develop-6-50

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-const getRectangle = function(height: number, width: number): number {
-  return height * width;
-}
+// const getRectangle = (height: number, width: number): number => {
+//   return height * width;
+// };
 
+const getRectangle = (height: number, width: number): number => height * width;
 console.log(getRectangle(3, 5));
+
+const putHight = (height: number): void => console.log(`${height}`);
+putHight(3);
+
+const hello = (): void => console.log('hello!');
+hello();
