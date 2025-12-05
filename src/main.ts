7b53ba1f6c8e66848d16ec85e85aa928e34ac086
etranger2268 <shun.5.2268@gmail.com>
2025-12-06 00:33:16 +0900

    develop-6-53

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
-const displayMessage = (): void => console.log('Timeout!');
-setTimeout(displayMessage, 3000);
+const getRectangle1 = (height: number, width: number = 10): number => height * width;
+console.log(getRectangle1(3, 5));
+console.log(getRectangle1(3));
+
+// error
+// const getRectangle2 = (height: number = 10, width: number): number => height * width;
+// console.log(getRectangle2(3, 5));
+// console.log(getRectangle2(3));
