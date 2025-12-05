96022a1d974188a44c2d5a7663c56637dfa9e25d
etranger2268 <shun.5.2268@gmail.com>
2025-12-05 17:26:52 +0900

    develop-5-42

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,45 @@
-const num = 6;
-// if (num % 2 === 0) {
-//   if (num % 3 === 0) {
-//     console.log(`${num}は2の倍数、かつ3の倍数です`);
-//   }
-// }
+// 1 -> Gold
+// 2 -> Silver
+// 3 -> Bronze
+// 4 -> 4th
+// 5 -> 5th
+// それ以外の値 -> Other
 
-if (num % 2 === 0 && num % 3 === 0) {
-  console.log(`${num}は2の倍数、かつ3の倍数です`);
+const num: number = 1;
+
+// switch
+switch (num) {
+  case 1:
+    console.log('Gold');
+    break;
+  case 2:
+    console.log('Silver');
+    break;
+  case 3:
+    console.log('Bronze');
+    break;
+  case 4:
+    console.log('4th');
+    break;
+  case 5:
+    console.log('5th');
+    break;
+  default:
+    console.log('Other');
+    break;
+}
+
+// if
+if (num === 1) {
+  console.log('Gold');
+} else if (num === 2) {
+  console.log('Silver');
+} else if (num === 3) {
+  console.log('Bronze');
+} else if (num === 4) {
+  console.log('4th');
+} else if (num === 5) {
+  console.log('5th');
+} else {
+  console.log('Other');
 }
