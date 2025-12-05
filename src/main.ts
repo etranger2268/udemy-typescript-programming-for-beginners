4049b5c1a8bc36c740815e27890ee21dbe99d212
etranger2268 <shun.5.2268@gmail.com>
2025-12-06 00:24:24 +0900

    develop-6-49

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
-function getRectangle(height: number, width: number): number {
+const getRectangle = function(height: number, width: number): number {
   return height * width;
 }
 
-// console.log(getRectangle('abc', 5));
 console.log(getRectangle(3, 5));
-
-function hello(): void {
-  console.log('Hello!');
-}
-
-hello();
