009f2571b843ff1fb1689e41d47dfb45a8d75c69
etranger2268 <shun.5.2268@gmail.com>
2025-12-06 06:57:20 +0900

    develop-8-62

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
-for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < 3; j++) {
-    console.log(`i: ${i}, j: ${j}`);
-  }
+let i = 1;
+while (i <= 3) {
+  console.log(i);
+  i++;
 }
-const scores: number[][] = [
-  [100, 99, 98],
-  [90, 89, 88],
-  [80, 79, 78],
-];
 
-console.log(scores);
-
-// for (let i = 0; i < scores.length; i++) {
-//   for (let j = 0; j < scores[i].length; j++) {
-//     console.log(scores[i][j]);
-//   }
-// }
+let j = 1;
+while (j <= 10) {
+  console.log(j);
+  if (j === 5) break;
+  j++;
+}
