158ce424effae1fa2ee3fabf9cd96027e32a31fa
etranger2268 <shun.5.2268@gmail.com>
2025-12-06 00:44:25 +0900

    develop-7-56

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,38 @@
 const colors: string[] = ['Red', 'Green', 'Blue'];
 console.log(colors);
 
-// const nums: string[] = [1, 2, 3];
+colors.push('Black');
+console.log(colors);
 
-const mixedArr = ['abc', 1, true];
-console.log(mixedArr);
+colors.unshift('White');
+console.log(colors);
 
-console.log(colors[0]);
-console.log(colors[1]);
-console.log(colors[2]);
-// console.log(colors[3]); // undefined
+colors[1] = 'Aka';
+console.log(colors);
 
-console.log(colors.length);
-console.log(colors[colors.length - 1]);
+const insertArr: string[] = ['a', 'b', 'c'];
+insertArr.splice(1, 0, 'D');
+console.log(insertArr);
+insertArr.splice(2, 0, 'X', 'Y', 'Z');
+console.log(insertArr);
 
-const emptyArr: number[] = [];
-console.log(emptyArr);
+insertArr.splice(2, 2);
+console.log(insertArr);
+
+const arr1: string[] = ['a', 'b', 'c'];
+const arr2: string[] = ['e', 'f', 'g'];
+const mergeArr = arr1.concat(arr2);
+console.log(mergeArr);
+console.log(arr1);
+console.log(arr2);
+
+const arr3: string[] = ['a', 'b', 'c'];
+arr3.shift();
+console.log(arr3);
+
+arr3.pop();
+console.log(arr3);
+
+const arr4: number[] = [1, 2, 3];
+const arr5: number[] = [4, 5, 6, ...arr4];
+console.log(arr5);
