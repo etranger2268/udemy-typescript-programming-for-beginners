40d2e0655b224a5a41dad4eacb9a4f66e4b5d2c5
etranger2268 <shun.5.2268@gmail.com>
2025-12-06 00:15:06 +0900

    develop-5-45

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,4 @@
-// 1 -> 大吉
-// 2 -> 中吉
-// 3 -> 小吉
-// 4 -> 吉
-// 5 -> 凶
-// それ以外 -> 想定外の数値です。
+const age = 20;
+const drink = age >= 20 ? 'Sake' : 'Cola';
 
-const num: number = Math.floor(Math.random() * 5) + 1;
-
-switch (num) {
-  case 1:
-    console.log('大吉');
-    break;
-  case 2:
-    console.log('中吉');
-    break;
-  case 3:
-    console.log('小吉');
-    break;
-  case 4:
-    console.log('吉');
-    break;
-  case 5:
-    console.log('凶');
-    break;
-  default:
-    console.log('想定外の数値です。');
-    break;
-}
+console.log(drink);
