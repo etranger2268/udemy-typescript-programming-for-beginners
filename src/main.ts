d0df4a4dd240092489e8ac033662115d33c49ed0
etranger2268 <shun.5.2268@gmail.com>
2025-12-06 07:00:05 +0900

    develop-8-64

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-let i = 1;
-do {
-  console.log(i);
-  i++;
-} while (i <= 3);
+const colors: string[] = ['Red', 'Green', 'Blue'];
+for (const color of colors) console.log(color);
+
+const str = 'TypeScript';
+for (const char of str) console.log(char);
