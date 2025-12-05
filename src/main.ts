3ff0ef0cee5fc86dad470d6ac1ad144cb0b284a5
etranger2268 <shun.5.2268@gmail.com>
2025-12-06 07:16:59 +0900

    develop-9-72

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,18 @@
 class User {
-  readonly name: string = '';
-  age: number = 0;
+  name: string = '';
+  private age: number = 0;
 
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
 
-  isAdult(): boolean {
+  public isAdult(): boolean {
     return this.age >= 18;
   }
-
-  // setName(name: string) {
-  //   this.name = name;
-  // }
 }
 
 const user = new User('etranger2268', 26);
 console.log(user.name);
-console.log(user.age);
+// console.log(user.age);
 console.log(user.isAdult());
