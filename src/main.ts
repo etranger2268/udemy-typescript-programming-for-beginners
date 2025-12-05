f1657f71ea56176ea1a2f8ac7aa846fb1e1600d6
etranger2268 <shun.5.2268@gmail.com>
2025-12-06 07:09:44 +0900

    develop-9-70

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 class User {
-  readonly name: string = 'etranger2268'
+  name: string = '';
   age: number = 0;
+  isAdult(): boolean {
+    return this.age >= 18;
+  }
+  setAge(age: number) {
+    this.age = age;
+  }
 }
 
 const user = new User();
-console.log(user);
-// user.name = 'etranger2268';
+console.log(user.age);
+console.log(user.isAdult());
+
+user.setAge(26);
+console.log(user.age);
+console.log(user.isAdult());
+
+const liam = new User();
+liam.setAge(18);
+console.log(liam.age);
+console.log(liam.isAdult());
